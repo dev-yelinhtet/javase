92329a4fc646b02ed4aebbc71b80d72b42b1ad68
YeLinDev <111472503+dev-yelinhtet@users.noreply.github.com>
2022-08-25 08:41:16 +0630

    second-commit

--- src/basic/Variable.java
+++ src/basic/Variable.java
@@ -3,6 +3,32 @@ package basic;
 public class Variable {
 
     public static void main(String[] args){
+        int age = 21;
+        float PI = 3.12f;
 
+        char gender = 'F';
+        String name = "John";
+
+        float weight;
+        weight = 160.8f;
+
+        int a = 6;
+        int b = 4;
+        int c = a + b;
+        System.out.println("The Value of C is " + c);
+
+        int d = a - b;
+        System.out.println("The Value of D is " + d);
+
+        int e = a * b;
+        System.out.println("The Value of E is " + e);
+
+        int f = a / b;
+        System.out.println("The Value of F is " + f);
+
+        int g= a % b;
+        System.out.println("The Value of G is " + g);
+
+        
     }
 }
